c8f522830dfe00c875f755cca82dec6808c940e3
= <carter.noh08@gmail.com>
2023-05-15 13:26:30 -0600

    Finished Camera Tutorial, testing on Amiga

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
-# Copyright (c) farm-ng, inc. Amiga Development Kit License, Version 0.1
+# Copyright (c) farm-ng, inc.
+#
+# Licensed under the Amiga Development Kit License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/farm-ng/amiga-dev-kit/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import argparse
 import asyncio
 import os
 from typing import List
 
-from amiga_package import ops
+import grpc
+from farm_ng.oak import oak_pb2
+from farm_ng.oak.camera_client import OakCameraClient
+from farm_ng.service import service_pb2
+from farm_ng.service.service_client import ClientConfig
+from turbojpeg import TurboJPEG
 
-# import internal libs
-
-# Must come before kivy imports
 os.environ["KIVY_NO_ARGS"] = "1"
 
 # gui configs must go before any other kivy import
@@ -24,17 +38,21 @@ Config.set("kivy", "keyboard_mode", "systemanddock")
 # kivy imports
 from kivy.app import App  # noqa: E402
 from kivy.lang.builder import Builder  # noqa: E402
+from kivy.graphics.texture import Texture  # noqa: E402
 
 
-class TemplateApp(App):
+class CameraApp(App):
     """Base class for the main Kivy app."""
 
-    def __init__(self) -> None:
+    def __init__(self, address: str, port: int, stream_every_n: int) -> None:
         super().__init__()
 
-        self.counter: int = 0
+        self.address = address
+        self.port = port
+        self.stream_every_n = stream_every_n
 
-        self.async_tasks: List[asyncio.Task] = []
+        self.image_decoder = TurboJPEG()
+        self.tasks: List[asyncio.Task] = []
 
     def build(self):
         return Builder.load_file("res/main.kv")
@@ -48,39 +66,95 @@ class TemplateApp(App):
             # we don't actually need to set asyncio as the lib because it is
             # the default, but it doesn't hurt to be explicit
             await self.async_run(async_lib="asyncio")
-            for task in self.async_tasks:
+            for task in self.tasks:
                 task.cancel()
 
-        # Placeholder task
-        self.async_tasks.append(asyncio.ensure_future(self.template_function()))
+        # configure the camera client
+        config = ClientConfig(address=self.address, port=self.port)
+        client = OakCameraClient(config)
 
-        return await asyncio.gather(run_wrapper(), *self.async_tasks)
+        # Stream camera frames
+        self.tasks.append(asyncio.ensure_future(self.stream_camera(client)))
 
-    async def template_function(self) -> None:
-        """Placeholder forever loop."""
+        return await asyncio.gather(run_wrapper(), *self.tasks)
+
+    async def stream_camera(self, client: OakCameraClient) -> None:
+        """This task listens to the camera client's stream and populates the tabbed panel with all 4 image streams
+        from the oak camera."""
         while self.root is None:
             await asyncio.sleep(0.01)
 
-        while True:
-            await asyncio.sleep(1.0)
+        response_stream = None
 
-            # increment the counter using internal libs and update the gui
-            self.counter = ops.add(self.counter, 1)
-            self.root.ids.counter_label.text = (
-                f"{'Tic' if self.counter % 2 == 0 else 'Tac'}: {self.counter}"
-            )
+        while True:
+            # check the state of the service
+            state = await client.get_state()
+
+            if state.value not in [
+                service_pb2.ServiceState.IDLE,
+                service_pb2.ServiceState.RUNNING,
+            ]:
+                # Cancel existing stream, if it exists
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None
+                print("Camera service is not streaming or ready to stream")
+                await asyncio.sleep(0.1)
+                continue
+
+            # Create the stream
+            if response_stream is None:
+                response_stream = client.stream_frames(every_n=self.stream_every_n)
+
+            try:
+                # try/except so app doesn't crash on killed service
+                response: oak_pb2.StreamFramesReply = await response_stream.read()
+                assert response and response != grpc.aio.EOF, "End of stream"
+            except Exception as e:
+                print(e)
+                response_stream.cancel()
+                response_stream = None
+                continue
+
+            # get the sync frame
+            frame: oak_pb2.OakSyncFrame = response.frame
+
+            # get image and show
+            for view_name in ["rgb", "disparity", "left", "right"]:
+                # Skip if view_name was not included in frame
+                try:
+                    # Decode the image and render it in the correct kivy texture
+                    img = self.image_decoder.decode(
+                        getattr(frame, view_name).image_data
+                    )
+                    texture = Texture.create(
+                        size=(img.shape[1], img.shape[0]), icolorfmt="bgr"
+                    )
+                    texture.flip_vertical()
+                    texture.blit_buffer(
+                        img.tobytes(),
+                        colorfmt="bgr",
+                        bufferfmt="ubyte",
+                        mipmap_generation=False,
+                    )
+                    self.root.ids[view_name].texture = texture
+
+                except Exception as e:
+                    print(e)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="template-app")
-
-    # Add additional command line arguments here
-
+    parser = argparse.ArgumentParser(prog="amiga_camera_tutorial")
+    parser.add_argument("--port", type=int, required=True, help="The camera port.")
+    parser.add_argument("--address", type=str, default="localhost", help="The camera address")
+    parser.add_argument("--stream-every-n", type=int, default=1, help="Streaming frequency")
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(TemplateApp().app_func())
+        loop.run_until_complete(
+            CameraApp(args.address, args.port, args.stream_every_n).app_func()
+        )
     except asyncio.CancelledError:
         pass
     loop.close()
